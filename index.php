94eca494ae1bd5ce9272ca26acadec3110718350
Diana Boudy <diana.boudy@gmail.com>
2018-11-05 15:35:44 +0100

    starting the architecture according to the mvc template, create directories and templates, trying bootstrap in home

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Liliaze
+ * Date: 01/11/2018
+ * Time: 14:50
+ */
+phpinfo();
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link href="../public/css/bootstrap.min.css" rel="stylesheet">
+    <title>Jean Forteroche Blog</title>
+
+</head>
+<body>
+    <h1>HELLO EVERYBODY</h1>
+    <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+    <script src="../public/js/jquery.min.js"></script>
+    <script src="../public/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ view/frontend/header.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Liliaze
+ * Date: 05/11/2018
+ * Time: 14:47
+ */
