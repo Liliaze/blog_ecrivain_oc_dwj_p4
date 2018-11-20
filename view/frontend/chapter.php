dccbbc2f7f5f5f5e69b4dd8c1b281900d3c9fde3
Diana Boudy <diana.boudy@gmail.com>
2018-11-20 14:52:25 +0100

    adding the next and previous buttons at the end of the chapter

--- view/frontend/chapter.php
+++ view/frontend/chapter.php
@@ -27,7 +27,7 @@ ob_start(); ?>
 
         <p><?= nl2br($data['content']) ?> <br/></p>
     </div>
-    <?php if (isset($_SESSION['login']) && $_SESSION['login'] != '') { ?>
+        <?php if (isset($_SESSION['login']) && $_SESSION['login'] != '') { ?>
         <div class="postComment col-lg-12">
             <form action="index.php?action=addComment&amp;idChapter=<?= $data['id'] ?>" method="post">
                 <fieldset id="fieldsetLoginForm">
@@ -42,45 +42,53 @@ ob_start(); ?>
                 </fieldset>
             </form>
         </div>
-    <?php } else { ?>
-        <div class="postComment col-lg-12">
-            <h3>Connectez-vous pour poster un commentaire :</h3>
-            <a href="index.php?action=login"><button>Connexion au site</button></a>
-        </div>
-    <?php }
-    }
-    $this->_oneChapter->closeCursor();?>
-    <div class="comments col-lg-12">
-        <h2>Commentaires :</h2>
-        <?php if ($this->_commentsChapterList->rowCount() == 0) {?>
-            <div class="oneComment">
-                <p class="bottom">Pas de commentaires pour le moment, soyez le premier !</p>
+        <?php } else { ?>
+            <div class="postComment col-lg-12">
+                <h3>Connectez-vous pour poster un commentaire :</h3>
+                <a href="index.php?action=login"><button>Connexion au site</button></a>
             </div>
         <?php }
-            while ($data= $this->_commentsChapterList->fetch()) {?>
+        $this->_oneChapter->closeCursor();?>
+        <div class="comments col-lg-12">
+            <h2>Commentaires :</h2>
+            <?php if ($this->_commentsChapterList->rowCount() == 0) {?>
                 <div class="oneComment">
-                    <h4>
-                        Posté par <em><?= htmlspecialchars($data['login']) ?></em>
-                        , le <?= $data['comment_date_fr'] ?>
-                    </h4>
-                    <p>
-                        <?= nl2br(htmlspecialchars($data['comment'])) ?> <br />
-                    </p>
-                    <ul class="menuComment bottom">
-                        <li class="like leftLi"><?= $data['nbLike'] ?></li>
-                        <li class="myLikeButton"><a href="index.php?action=likeComment&amp;idChapter=<?= $data['idChapter'] ?>&amp;idComment=<?= $data['id'] ?>"><i class="far fa-thumbs-up"></i></a></li>
-                        <li class="dislike"><?= $data['nbDislike'] ?></li>
-                        <li class="dislikeButton"><a href="index.php?action=unlikeComment&amp;idChapter=<?= $data['idChapter'] ?>&amp;idComment=<?= $data['id']?> "><i class="far fa-thumbs-down"></i></a></li>
-                        <?php if ($data['signaled'] == 0) {?>
-                            <li class="signaledButton rightLi"><a href="index.php?action=signaledComment&amp;idChapter=<?= $data['idChapter'] ?>&amp;idComment=<?= $data['id'] ?>"><i class="fas fa-door-closed">&nbspsignaler</i></a></li>
-                        <?php } else if ($data['signaled'] == 1 && $data['manualApprove'] == 0) {?>
-                        <li class="signaled rightLi"><i class="fas fa-door-closed">&nbspCommentaire signalé, en attente de modération</i></li>
-                        <?php }  else if ($data['signaled'] == 1 && $data['manualApprove'] == 1) {?>
-                        <li class="approve rightLi"><i class="fas fa-check">&nbspCommentaire approuvé suite à signalement</i></li>
-                    <?php } ?>
-                    </ul>
+                    <p class="bottom">Pas de commentaires pour le moment, soyez le premier !</p>
                 </div>
-        <?php }?>
+            <?php }
+                while ($data= $this->_commentsChapterList->fetch()) {?>
+                    <div class="oneComment">
+                        <h4>
+                            Posté par <em><?= htmlspecialchars($data['login']) ?></em>
+                            , le <?= $data['comment_date_fr'] ?>
+                        </h4>
+                        <p>
+                            <?= nl2br(htmlspecialchars($data['comment'])) ?> <br />
+                        </p>
+                        <ul class="menuComment bottom">
+                            <li class="like leftLi"><?= $data['nbLike'] ?></li>
+                            <li class="myLikeButton"><a href="index.php?action=likeComment&amp;idChapter=<?= $data['idChapter'] ?>&amp;idComment=<?= $data['id'] ?>"><i class="far fa-thumbs-up"></i></a></li>
+                            <li class="dislike"><?= $data['nbDislike'] ?></li>
+                            <li class="dislikeButton"><a href="index.php?action=unlikeComment&amp;idChapter=<?= $data['idChapter'] ?>&amp;idComment=<?= $data['id']?> "><i class="far fa-thumbs-down"></i></a></li>
+                            <?php if ($data['signaled'] == 0) {?>
+                                <li class="signaledButton rightLi"><a href="index.php?action=signaledComment&amp;idChapter=<?= $data['idChapter'] ?>&amp;idComment=<?= $data['id'] ?>"><i class="fas fa-door-closed">&nbspsignaler</i></a></li>
+                            <?php } else if ($data['signaled'] == 1 && $data['manualApprove'] == 0) {?>
+                            <li class="signaled rightLi"><i class="fas fa-door-closed">&nbspCommentaire signalé, en attente de modération</i></li>
+                            <?php }  else if ($data['signaled'] == 1 && $data['manualApprove'] == 1) {?>
+                            <li class="approve rightLi"><i class="fas fa-check">&nbspCommentaire approuvé suite à signalement</i></li>
+                        <?php } ?>
+                        </ul>
+                    </div>
+            <?php }?>
+            <div class="linkOtherChapter">
+                <?php if ($this->_previousIdChapter != null) { ?>
+                    <a href="index.php?action=chapter&amp;idChapter=<?= $this->_previousIdChapter ?>"><button><i class="fas fa-angle-double-left"></i>&nbspCHAPITRE&nbspPRECEDENT</button></a>
+                <?php } ?>
+                <?php if ($this->_nextIdChapter != null) { ?>
+                    <a href="index.php?action=chapter&amp;idChapter=<?= $this->_nextIdChapter?>"><button><i class="fas fa-angle-double-right"></i>&nbspCHAPITRE&nbspSUIVANT</button></a>
+                <?php } ?>
+            </div>
+        <?php  } ?>
     </div>
 </div>
 <?php
