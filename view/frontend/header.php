0562af861a347a1f03ac2284908cb699510b5c44
Diana Boudy <diana.boudy@gmail.com>
2018-11-26 11:46:01 +0100

    little screen

--- view/frontend/header.php
+++ view/frontend/header.php
@@ -1,16 +1,16 @@
 
 <header>
-    <ul class="col-lg-12" id="menu">
-        <li class="button_left col-lg-1 col-md-2 col-xs-2"><a href="index.php?action=home"><i class="fas fa-home">&nbsp;Accueil</i></a></li>
-        <li class="button_left col-lg-1 col-md-2 col-xs-2"><a href="index.php?action=chapterList"><i class="fas fa-book-reader">&nbsp;Chapitres</i></a></li>
-        <li class="button_left col-lg-1 col-md-2 col-xs-2"><a href="index.php?action=author"><i class="fas fa-user-alt">&nbsp;L'auteur</i></a></li>
+    <ul class="col-lg-12 col-md-12 col-xs-12" id="menu">
+        <li class="button_left col-lg-1 col-md-2 col-xs-12"><a href="index.php?action=home"><i class="fas fa-home">&nbsp;Accueil</i></a></li>
+        <li class="button_left col-lg-1 col-md-2 col-xs-12"><a href="index.php?action=chapterList"><i class="fas fa-book-reader">&nbsp;Chapitres</i></a></li>
+        <li class="button_left col-lg-1 col-md-2 col-xs-12"><a href="index.php?action=author"><i class="fas fa-user-alt">&nbsp;L'auteur</i></a></li>
         <?php if (isset($_SESSION['id']) && $_SESSION['id']  == ''  || !isset($_SESSION['login'])) {?>
-        <li class="button_right col-lg-offset-7 col-md-offset-0 col-lg-1 col-md-2 col-xs-2"><a href="index.php?action=login"><i class="fas fa-unlock-alt">&nbsp;Connexion</i></a></li>
+        <li class="button_right col-lg-offset-7 col-md-offset-0 col-lg-1 col-md-2 col-xs-12"><a href="index.php?action=login"><i class="fas fa-unlock-alt">&nbsp;Connexion</i></a></li>
         <?php } else {?>
-        <li class="button_right col-lg-offset-7 col-md-offset-0 col-lg-1 col-md-2 col-xs-2"><a href="index.php?action=logout"><i class="fas fa-unlock-alt">Déconnexion</i></a></li>
+        <li class="button_right col-lg-offset-7 col-md-offset-0 col-lg-1 col-md-2 col-xs-12"><a href="index.php?action=logout"><i class="fas fa-unlock-alt">&nbsp;Déconnexion</i></a></li>
         <?php } ?>
         <?php if (isset($_SESSION['admin']) && $_SESSION['admin'] == 1) {?>
-        <li class="button_right col-lg-1 col-md-2 col-xs-2"><a href="index.php?action=admin_home"><i class="fas fa-hammer">&nbsp;Admin</i></a></li>
+        <li class="button_right col-lg-1 col-md-2 col-xs-12"><a href="index.php?action=admin_home"><i class="fas fa-hammer">&nbsp;Admin</i></a></li>
         <?php } ?>
     </ul>
 </header>
--- view/frontend/template.php
+++ view/frontend/template.php
@@ -6,6 +6,7 @@
     <link href="public/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link href="public/css/style-lg.css" rel="stylesheet">
+    <link media="all and (max-width: 768px)" href="public/css/style-little_screen.css" rel="stylesheet">
     <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=2ajf9vx4rr90q9n1ws6szrajzywfiwsyyc7m4l6camx77iur"></script>
     <script>
         tinymce.init({
@@ -14,16 +15,14 @@
     </script>
     <title>Jean Forteroche</title>
 </head>
-<body class="col-lg-12">
+<body class="col-lg-12 col-md-12 col-xs-12">
     <?php require('view/frontend/alert.php'); ?>
     <?php require('view/frontend/header.php'); ?>
-    <div id="content" class="col-lg-12"><br/>
+    <div id="content" class="col-lg-12 col-md-12 col-xs-12"><br/>
         <?= $alert?>
         <?= $content?>
     </div>
     <script src="public/js/formAddComment.js"></script>
-    <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
-    <script src="public/js/jquery.min.js"></script>
     <script src="public/js/bootstrap.min.js"></script>
     <?php require('view/frontend/footer.php'); ?>
 </body>
